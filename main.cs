42643450bc26b8681969821bc24bef3122731c7f
tarzan1376 <tarzan1376@gmail.com>
2024-09-23 23:26:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+
+    Console.WriteLine("What is the minimum sustainted wind speed? : ");
+      double windSpeed;
+      double.TryParse(Console.ReadLine(), out windSpeed);
+
+    
+    if (windSpeed < 74) 
+    {
+        Console.WriteLine("Not a Hurricane");
+    }
+    else if (windSpeed >= 74 && windSpeed < 96) 
+    {
+        Console.WriteLine("Category One Hurricane");
+    }
+    else if (windSpeed >= 96 && windSpeed < 111)
+    {
+       Console.WriteLine("Category Two Hurricane");
+    }
+    else if (windSpeed >= 111 && windSpeed < 130)
+      {
+        Console.WriteLine("Category Three Hurricane");
+      }
+    else if (windSpeed >= 130 && windSpeed < 157)
+      {
+        Console.WriteLine("Category Four Hurricane");
+      }
+    else if (windSpeed >= 157)
+    {
+      Console.WriteLine("Category Five Hurricane");
+    }
+    
+  
+  }
+}
